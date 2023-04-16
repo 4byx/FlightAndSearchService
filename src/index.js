628d53e90e90f10008468eacb67905b02f880d7f
Abhimanyu Ahuja <iabhimanyu.ahuja@gmail.com>
2023-04-16 20:49:39 +0530

    setup sequelize and made folders

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ const { PORT } = require("./config/serverConfig");
 
 const SetupAndStartServer = async () => {
   const app = express();
+
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
   app.listen(PORT, () => {
     console.log("server started at port ", PORT);
   });
