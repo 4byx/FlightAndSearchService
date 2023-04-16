023952a5bf3f085b2f20d28f67055bf6cf1dac09
Abhimanyu Ahuja <iabhimanyu.ahuja@gmail.com>
2023-04-16 15:08:50 +0530

    setup a basic server

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
-const express = require('express');
- 
+const express = require("express");
+const bodyParser = require("body-parser");
 
-
-
-const SetupAndStartServer = async () = {
-
-    // app.listen(PORTm)
-    
-}
-
-
+const SetupAndStartServer = async () => {
+  const app = express();
+  const PORT = 3000;
+  app.listen(PORT, () => {
+    console.log("server started at port ", PORT);
+  });
+};
 
 SetupAndStartServer();
