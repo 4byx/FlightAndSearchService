450145f6fd7f132fba2915ed8ef3a132804fadd1
Abhimanyu Ahuja <iabhimanyu.ahuja@gmail.com>
2023-04-18 23:05:38 +0530

    completed city repository

--- src/repository/city-respository.js
+++ src/repository/city-respository.js
@@ -1,23 +1,53 @@
 const { City } = require("../models/index");
 
+// CRUD
 class CityRespository {
   async createCity({ name }) {
     try {
-      const city = await City.create({ name });
+      const city = await City({
+        name,
+      });
       return city;
     } catch (err) {
+      console.log("Some error in the respository layer");
       throw { err };
     }
   }
 
-  async deleteCity({ cityId }) {
+  async deleteCity(cityId) {
     try {
       await City.destroy({
         where: {
           id: cityId,
         },
       });
+      return true;
     } catch (err) {
+      console.log("Some error in the repository layer");
+      throw { err };
+    }
+  }
+
+  async updateCity(cityId, data) {
+    try {
+      const city = await City.update(data, {
+        where: {
+          id: cityId,
+        },
+      });
+      return city;
+    } catch (err) {
+      console.log("Some error in the repository layer");
+      throw { err };
+    }
+  }
+
+  async getCity(cityId) {
+    try {
+      const city = await City.findbyPK(cityId);
+      return city;
+    } catch (err) {
+      console.log("Some error in the repository layer");
       throw { err };
     }
   }
