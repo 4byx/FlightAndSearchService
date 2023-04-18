4e4f9975bbebf07c9a76289adeb56a99e737f7a7
Abhimanyu Ahuja <iabhimanyu.ahuja@gmail.com>
2023-04-18 08:04:49 +0530

    added unqiue constraint and started cityRespository

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const CityRespository = require("./repository/city-respository");
 const { PORT } = require("./config/serverConfig");
 
 const SetupAndStartServer = async () => {
@@ -10,6 +10,8 @@ const SetupAndStartServer = async () => {
   app.use(bodyParser.urlencoded({ extended: true }));
   app.listen(PORT, () => {
     console.log("server started at port ", PORT);
+    const repo = new CityRespository();
+    repo.createCity({ name: "New Delhi" });
   });
 };
 
