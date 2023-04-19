b843bb4d97782ba85e008017dba577ff03b66f7b
Abhimanyu Ahuja <iabhimanyu.ahuja@gmail.com>
2023-04-19 22:06:28 +0530

    added city service

---
+++ src/services/city-service.js
@@ -0,0 +1,45 @@
+const { CityRepository } = require("../repositories/index");
+
+class CityService {
+  constructor() {
+    this.cityRepository = new CityRepository();
+  }
+
+  async createCity(data) {
+    try {
+      const city = this.cityRepository.createCity(data);
+      return city;
+    } catch (error) {
+      console.log("Something wrong at service layer");
+      throw { error };
+    }
+  }
+  async deleteCity(cityId) {
+    try {
+      await this.cityRepository.deleteCity(cityId);
+    } catch (error) {
+      console.log("Something wrong at service layer");
+      throw { error };
+    }
+  }
+
+  async updateCity(cityId, data) {
+    try {
+      const city = await this.cityRepository.updateCity(cityId, data);
+      return city;
+    } catch (error) {
+      console.log("Something wrong at service layer");
+      throw { error };
+    }
+  }
+
+  async getCity(cityId) {
+    try {
+      const city = await this.cityRepository.getCity(cityId);
+      return city;
+    } catch (error) {
+      console.log("Something wrong at service layer");
+      throw { error };
+    }
+  }
+}
